705c4e59660b1cc76d232a9fd8e30de0a15c2233
kirat14 <moumini.tarik@gmail.com>
2026-09-07 12:32:15 -0500

    fix: GetLeague issue

--- Services/LeagueService.cs
+++ Services/LeagueService.cs
@@ -95,7 +95,6 @@ class LeagueService : ILeagueService
     public async Task<LeagueResponse> GetLeague(int id)
     {
         var league = await _context.Leagues
-        .Include(l => l.Teams)
         .FirstOrDefaultAsync(l => l.Id == id);
 
         if (league == null)
