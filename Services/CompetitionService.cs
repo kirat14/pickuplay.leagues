62217af9c1bc4dd4aa17197d61aa0f9ed3897d5e
kirat14 <moumini.tarik@gmail.com>
2026-09-19 16:20:33 -0500

    fix: throw team not found exception when updating competition entry

--- Services/CompetitionService.cs
+++ Services/CompetitionService.cs
@@ -326,6 +326,14 @@ class CompetitionService : ICompetitionService
             throw new CompetitionTeamEntryNotFoundException();
         }
 
+        var team = await _context.Teams
+            .FirstOrDefaultAsync(t => t.Id == entry.TeamId);
+
+
+
+        if (team == null)
+            throw new TeamNotFoundException();
+
         competition_entry.Status = entry.Status ?? competition_entry.Status;
         competition_entry.TeamId = entry.TeamId > 0 ? entry.TeamId : competition_entry.TeamId;
         competition_entry.Comment = entry.Comment ?? competition_entry.Comment;
